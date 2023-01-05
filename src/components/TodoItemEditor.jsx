e845dda1c261d4cb75ba4ef00f9c461f0966c3b7
takat <takatsuka.masaya0914@gmail.com>
2023-01-05 20:37:07 +0900

    review by takatsuka

--- src/components/TodoItemEditor.jsx
+++ src/components/TodoItemEditor.jsx
@@ -1,51 +1,49 @@
-import { useState } from 'react';
-import { useSetRecoilState, useRecoilState, useRecoilValue } from 'recoil';
-import { todoListState, todoIsEditable, todoEditId, todoEditTitle } from '../components/atom';
+import { useState } from "react";
+import { useSetRecoilState, useRecoilState, useRecoilValue } from "recoil";
 import {
-  Flex,
-  Input,
-  Button
-} from '@chakra-ui/react'
-import { CheckIcon } from '@chakra-ui/icons'
+  todoListState,
+  todoIsEditable,
+  todoEditId,
+  todoEditTitle,
+} from "../components/atom";
+import { Flex, Input, Button } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
 
-function TodoItemEditor({todoList}) {
+function TodoItemEditor({ todoList }) {
   const [title, setTitle] = useRecoilState(todoEditTitle);
+  // ↓親コンポーネントからpropsで持ってきましょう
   const setTodoList = useSetRecoilState(todoListState);
   const [startDate, setStartDate] = useState(new Date());
   const handleChange = (e) => {
     setTitle(e.target.value);
-    console.log(title)
+    console.log(title);
   };
 
   // 編集対象のtodoId
   const setEditId = useRecoilValue(todoEditId);
+  // set関数は使用している形跡がありますが、stateであるisEditableが使われていないということはstate管理する必要がないのではと思うのですが、、、
   const [isEditable, setIsEditable] = useRecoilState(todoIsEditable);
   const closeEditTodo = () => {
-
     const newArray = todoList.map((item) =>
-    item.id === setEditId ? { ...item, title: title } : item
-  )
-    setTodoList(newArray)
-    setIsEditable(false)
-    setTitle('')
-  }
+      item.id === setEditId ? { ...item, title: title } : item
+    );
+    setTodoList(newArray);
+    setIsEditable(false);
+    setTitle("");
+  };
 
   const handleKeyDown = (e) => {
-    if (e.key === 'Enter') 
-    closeEditTodo(e)
-  }
+    if (e.key === "Enter") closeEditTodo(e);
+  };
 
   return (
-    <Flex minWidth='max-content' alignItems='center' gap='2' mb={8} >
-      <Input
-      value={title}
-      onChange={handleChange}
-      onKeyDown={handleKeyDown} 
-      />
-      <Button colorScheme='green' onClick={closeEditTodo}><CheckIcon/></Button>
+    <Flex minWidth="max-content" alignItems="center" gap="2" mb={8}>
+      <Input value={title} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <Button colorScheme="green" onClick={closeEditTodo}>
+        <CheckIcon />
+      </Button>
     </Flex>
   );
 }
 
 export default TodoItemEditor;
-
